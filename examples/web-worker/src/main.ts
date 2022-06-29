55202fcb3fa991a05c86a0178df1e7c6463b270c
kazk <kazk.dev@gmail.com>
2022-06-28 18:21:31 -0700

    Simplify WebWorker example

--- examples/web-worker/src/main.ts
+++ examples/web-worker/src/main.ts
@@ -10,77 +10,11 @@ import "codemirror/addon/hint/show-hint";
 import "codemirror/addon/edit/matchbrackets";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/addon/runmode/runmode";
-// import "codemirror/keymap/vim";
 
 import { marked } from "marked";
 
 import { Workspace } from "@qualified/codemirror-workspace";
 import "@qualified/codemirror-workspace/css/default.css";
-
-const modeMap: { [k: string]: string } = {
-  json: "application/json",
-  typescript: "text/typescript",
-  javascript: "text/javascript",
-  html: "text/html",
-  css: "text/css",
-};
-
-const highlight = (code: string, language: string) => {
-  const mode = modeMap[language] || "text/plain";
-  const tmp = document.createElement("div");
-  CodeMirror.runMode(code, mode, tmp, { tabSize: 4 });
-  return tmp.innerHTML;
-};
-
-marked.use({
-  // @ts-ignore renderer can be object literal
-  renderer: {
-    code(code: string, language: string | undefined) {
-      if (!language) language = "text";
-      code = highlight(code, language);
-      // We need to add a class for the theme (e.g., `cm-s-idea`) on the wrapper.
-      // If we're using a custom theme, it can apply its styles to `code[class^="language-"]`
-      // and use Marked's default `code` with `highlight` option.
-      return `<pre><code class="cm-s-idea language-${language}">${code}</code></pre>`;
-    },
-  },
-});
-
-const sampleJson = JSON.stringify(
-  {
-    compilerOptions: {
-      target: "es2017",
-      module: "commonjs",
-      strict: true,
-      esModuleInterop: true,
-      skipLibCheck: true,
-      forceConsistentCasingInFileNames: true,
-    },
-  },
-  null,
-  2
-);
-
-const $ = (sel: string) => {
-  const el = document.querySelector(sel);
-  if (!el) throw new Error(`No element matching ${sel}`);
-  return el as HTMLElement;
-};
-
-const config: CodeMirror.EditorConfiguration = {
-  theme: "idea",
-  // keyMap: "vim",
-  gutters: ["cmw-gutter"],
-  lineNumbers: true,
-  matchBrackets: true,
-  autoCloseBrackets: true,
-};
-
-const jsonEditor = CodeMirror($("#editor"), {
-  ...config,
-  mode: "application/json",
-  value: sampleJson,
-});
 
 const JSON_WORKER = "json-worker";
 const workspace = new Workspace({
@@ -103,4 +37,27 @@ const workspace = new Workspace({
   renderMarkdown: (markdown) => marked(markdown),
 });
 
-workspace.openTextDocument("tsconfig.json", jsonEditor);
+const editor = CodeMirror(document.getElementById("editor")!, {
+  theme: "idea",
+  gutters: ["cmw-gutter"],
+  lineNumbers: true,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  mode: "application/json",
+  value: JSON.stringify(
+    {
+      compilerOptions: {
+        target: "es2017",
+        module: "commonjs",
+        strict: true,
+        esModuleInterop: true,
+        skipLibCheck: true,
+        forceConsistentCasingInFileNames: true,
+      },
+    },
+    null,
+    2
+  ),
+});
+
+workspace.openTextDocument("tsconfig.json", editor);
